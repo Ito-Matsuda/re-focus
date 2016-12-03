f72e2e503a4be94bc2b0ea0a4f8726bf7bf21195
Jose Manuel (Ito) <ito-matsuda@hotmail.com>
2016-12-03 14:41:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-/* JavaScript */
+//This should interact with the html file
+
+//Global Variables
+var duration;
+
+/*
+Purpose of setTimer is to set the initial timer that
+every "x" minutes, you will get a popup / notification
+asking if you are focusing.
+*/
+function setTimer(sel){
+ 	// Takes time from a drop-down menu from the index.html file
+ 	if (sel.value == "short") {
+ 		duration = 15;
+ 	}
+ 	else if (sel.value == "medium"){
+ 		duration = 30;
+ 	}
+ 	else if (sel.value == "long"){
+ 		duration = 45;
+ 	}
+ 	else if (sel.value == "super-long"){
+ 		duration = 60;
+ 	}
+ 	//Wait for start button
+}
+
+function start(){
+
+}
+
+
 $( document ).ready(function() {
 	$("#next-step-section").remove();
 	$("#current-point-section").remove();
