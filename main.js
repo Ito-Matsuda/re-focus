5d38eb95d45b66da98876590e6bc0f86c8bfd144
Jose Manuel (Ito) <ito-matsuda@hotmail.com>
2016-12-06 17:25:16 -0500

    Added the timer functionality
    TODO:
    Nothing happens when the timer hits zero (other than initializing the timer to null)

--- main.js
+++ main.js
@@ -26,26 +26,10 @@ howStartListener.addEventListener("click", secondField);
 var currentPntListener = document.getElementById("currentPntButton");
 currentPntListener.addEventListener("click", thirdField);
 
+
+// Putting this event listener here seems to work
 var focusUpListener = document.getElementById("focusUpButton");
 focusUpListener.addEventListener("click", start);
-
-function setTimer(sel){
- 	// Takes time from a drop-down menu from the index.html file
- 	//Uses momentjs
- 	if (sel.value == "short") {
- 		timeLeft = moment.duration(15, 'minutes');
- 	}
- 	else if (sel.value == "medium"){
- 		timeLeft = moment.duration(30, 'minutes');
- 	}
- 	else if (sel.value == "long"){
- 		timeLeft = moment.duration(45, 'minutes');
- 	}
- 	else if (sel.value == "super-long"){
- 		timeLeft = moment.duration(60, 'minutes');
- 	}
- 	//Wait for start button
-}
 
 function firstField(){
 	obj = document.getElementById("objective").value;
@@ -56,7 +40,7 @@ function firstField(){
     $("#objective-section").fadeOut();
     $("#objective-section").addClass("hidden");
     $("#next-step-section").fadeIn();
-}
+} // End firstField
 
 function secondField(){
 	alert("got to second");
@@ -65,7 +49,7 @@ function secondField(){
 	$("#next-step-section").fadeOut();
     $("#next-step-section").addClass("hidden");
     $("#current-point-section").fadeIn();
-}
+} // End secondField
 
 function thirdField(){
 	alert("got to third");
@@ -77,10 +61,12 @@ function thirdField(){
     document.getElementById("first").append(obj);
     document.getElementById("second").append(nextStep);
     document.getElementById("third").append(cpoint);
-}
+} // End thirdField
 
 function start(){
 	alert("got to start")
+	var minutez = $("#timeMinutes").text();
+	var secondz = $("#timeSeconds").text();
     // Provides short, medium, long, super-long
     var timeChosen = document.getElementById("timer").value;
     if (timeChosen == "short"){
@@ -88,7 +74,6 @@ function start(){
     }
     else if (timeChosen == "medium"){
         timeLeft = moment.duration({minutes:30, seconds:0});
-        alert("LOL");
     }
     else if (timeChosen == "long"){
         timeLeft = moment.duration({minutes:45, seconds:0});
@@ -96,16 +81,31 @@ function start(){
     else if (timeChosen == "super-long"){
         timeLeft = moment.duration({minutes:60, seconds:0});
     }
-    //Need to have a timer, this runs every second, update display?
     document.getElementById("focusUpButton").disabled = true;
+    //Need to have a timer, this runs every second, update display?
     timer = window.setInterval(tickTheTime, 1000);
-}
+} // End start
 
 function tickTheTime(){
     timeLeft.subtract(1, 's');
 
-}
+    // Get remaining time
+    var min = timeLeft.get('minutes');
+    var sec = timeLeft.get('seconds');
+    $("#timeMinutes").text(min);
+    $("#timeSeconds").text(sec);
+    if(timeLeft.asSeconds() == 0){
+    	timerEnded();
+    }
+} // End tickTheTime
 
+function timerEnded(){
+	timer = null;
+	/* Maybe write a congratulations? 
+	Have it so that you can type in all your stuff again?
+	If you do that, make sure to re-init everything */
+	alert("It's time to take a break and re-focus!");
+} // End timerEnded
 
 
 function countDown(){
